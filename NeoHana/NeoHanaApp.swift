04b2598a371c0c9b9899afe83f27d55810111468
Zerui18 <161558w@student.hci.edu.sg>
2021-06-12 19:15:27 +0800

    Initial Commit

---
+++ NeoHana/NeoHanaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeoHanaApp.swift
+//  NeoHana
+//
+//  Created by Zerui Chen on 12/6/21.
+//
+
+import SwiftUI
+
+@main
+struct NeoHanaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
